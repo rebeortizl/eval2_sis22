8c96189a6edee0473712c04b2e569bae5bcd09d4
AdelaydaAlvarado <adelayda.henriquez22@itca.edu.sv>
2023-09-29 10:51:50 -0600

    prueba de conexion

--- lib/examen2.dart
+++ lib/examen2.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class eval2 extends StatelessWidget {
-  const eval2({super.key});
+class examen2 extends StatelessWidget {
+  const examen2({super.key});
 
   @override
   Widget build(BuildContext context) {
