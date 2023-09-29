ebde30c7e0fd8f9d2858bb8af3417d0243769e46
AdelaydaAlvarado <adelayda.henriquez22@itca.edu.sv>
2023-09-29 10:54:48 -0600

    prueba de conexion2

---
+++ lib/prueba2.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class prueba2 extends StatelessWidget {
+  const prueba2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
